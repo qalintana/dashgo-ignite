302aebeed9088ea749a732c97212779258468e6c
qalintana <alintana18@gmail.com>
2021-08-08 09:11:03 +0100

    criando _document

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,6 +1,21 @@
 import { extendTheme } from '@chakra-ui/react';
 
 export const theme = extendTheme({
+  colors: {
+    gray: {
+      '900': '#181b23',
+      '800': '#1f2029',
+      '700': '#353646',
+      '600': '#4B4D63',
+      '500': '#616480',
+      '400': '#797D9A',
+      '300': '#9699B0',
+      '200': '#B3B5C6',
+      '100': '#D1D2Dc',
+      '50': '#eeeef2',
+    },
+  },
+
   styles: {
     global: {
       body: {
