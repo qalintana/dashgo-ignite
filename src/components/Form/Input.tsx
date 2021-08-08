c35e973b4facfbff1b20d711552c8bebf8150804
qalintana <alintana18@gmail.com>
2021-08-08 11:18:28 +0100

    cabecalho e inputs do chakra-ui

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,30 @@
+import {
+  FormLabel,
+  FormControl,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from '@chakra-ui/react';
+
+interface IInputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: IInputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+
+      <ChakraInput
+        id={name}
+        name={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{ bgColor: 'gray.900' }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
